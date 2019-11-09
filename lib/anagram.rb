a001d502e875d17c9ee30227022869618f3bca33
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-09 23:53:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
       if @word.split("").sort != words.split("").sort
         return []
       elsif @word.split("").sort == words.split("").sort
-        return words.split("").join
+        return words.join.split("")
       end
       binding.pry
     end
