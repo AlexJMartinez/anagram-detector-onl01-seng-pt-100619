b1c99fddbb75713f45fa8f3140546d2b580bf23b
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-10 00:27:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,8 @@ class Anagram
   end
 
   def match(array)
+    return array.select { |word| word.split("").sort == @word.split("").sort }
+
     array.each do |words|
       if @word.split("").sort != words.split("").sort
         empty_array = []
